5610b557b5dad0a61fe074f9addc8f5c458d6a67
dahoon06 <dahoon226@naver.com>
2023-05-14 11:56:41 +0900

    feat: material ui 추가

--- app/src/assets/styles/all.stories.style.ts
+++ app/src/assets/styles/all.stories.style.ts
@@ -2,14 +2,29 @@ import { StyleSheet } from 'react-native';
 
 export const AllStoriesStyle = StyleSheet.create({
   Top: {
-    height: '100%',
-    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    flex: 0.3,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: "space-between",
   },
+  body: {
+    width: '100%',
+    flex: 1.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listWrapper: {
+    width: '100%',
+    textAlign: 'left',
+    height: '100%'
+  },
+  listItems: {
+    marginBottom: 30,
+  },
   bottom: {
-    flex: 1
+    flex: 0.3
   },
   button: {
     width: 'auto'
--- app/src/screen/AllStoryScreen.tsx
+++ app/src/screen/AllStoryScreen.tsx
@@ -1,26 +1,61 @@
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {RootStackParamList} from "@src/types/router";
-import {Text, View} from "react-native";
+import {ScrollView, Text, TouchableOpacity, View} from "react-native";
 import {AllStoriesStyle} from "@asset/styles/all.stories.style";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 type Props = NativeStackScreenProps<RootStackParamList, 'AllStory'>;
 
 export const AllStoryScreen = (props: Props) => {
+  const {navigation} = props;
+  const home = () => {
+    navigation.navigate("Home")
+  }
 
   return (
-    <View>
+    <View style={{
+      flex: 1
+    }}>
       <View style={AllStoriesStyle.Top}>
-        <Text>TEST1</Text>
-
+        <TouchableOpacity>
+          <Icon
+            size={24}
+            name="plus"  />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Icon name="dots-vertical" size={24} />
+        </TouchableOpacity>
       </View>
-      <View>
-        <Text>게시글 리스트</Text>
-        <Text>게시글 리스트</Text>
-        <Text>게시글 리스트</Text>
-        <Text>게시글 리스트</Text>
-        <Text>게시글 리스트</Text>
+      <View style={AllStoriesStyle.body}>
+        <ScrollView style={AllStoriesStyle.listWrapper}>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트dasfjs;ldfja</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트asfsajhfsk</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트dasfjs;ldfja</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트asfsajhfsk</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트dasfjs;ldfja</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트asfsajhfsk</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트dasfjs;ldfja</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트asfsajhfsk</Text>
+          <Text style={AllStoriesStyle.listItems}>게시글 리스트</Text>
+        </ScrollView>
       </View>
       <View style={AllStoriesStyle.bottom}>
+        <TouchableOpacity onPress={home}>
+          <Icon
+            size={24}
+            name="circle-outline"  />
+        </TouchableOpacity>
       </View>
     </View>
   )
