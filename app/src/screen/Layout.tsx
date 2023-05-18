cf6670e7bc48e7bd422025deada35bfa18cd5b05
dahoon06 <dahoon226@naver.com>
2023-05-18 13:46:42 +0900

    init: react-native status bar 로 교체

--- app/src/screen/Layout.tsx
+++ app/src/screen/Layout.tsx
@@ -1,18 +1,20 @@
 import {Router} from "@src/router";
 import {Header} from "@src/components/Header";
-import {StatusBar} from "expo-status-bar";
-import {View} from "react-native";
+import {View, StatusBar} from "react-native";
 
 export const Layout = () => {
   return (
     <>
-      <StatusBar style={"light"} />
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'transparent'}
+        translucent={true}  />
       <Header />
       <View style={{
         flex: 1,
         width: '100%',
         height: '100%',
-        paddingTop: 90,
+        paddingTop: 90
       }}>
         <Router />
       </View>
